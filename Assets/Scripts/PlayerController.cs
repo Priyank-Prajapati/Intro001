4fd7cc2a9d8d29527d3cd91b972b0c43df742725
Priyank <ppraja20@my.centennialcollege.ca>
2020-01-24 14:23:21 -0500

    Added Player Movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    int movementspeed=2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizon = Input.GetAxis("Horizontal");
+
+        GetComponent<Rigidbody2D>().velocity = new Vector2(horizon*movementspeed, 0.0f);
+    }
+}
