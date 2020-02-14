52d165c0803711bfbce510c05d4449f2795aa671
Priyank <ppraja20@my.centennialcollege.ca>
2020-02-14 13:27:54 -0500

    Added Laser

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    //Public variables
+    public float speed = 10;
+
+    //Private variables
+    private Rigidbody2D rbody;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rbody = GetComponent<Rigidbody2D>();
+        rbody.velocity = new Vector2(speed, 0);
+    }
+}
