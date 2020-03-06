9eab4124873b05a72c2983127a175cf81426b054
Priyank <ppraja20@my.centennialcollege.ca>
2020-03-06 13:55:43 -0500

    Added Explosion

---
+++ Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour
+{
+    public GameObject Explosion;
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        Instantiate(Explosion, transform.position, transform.rotation);
+
+        Destroy(other.gameObject);
+        Destroy(this.gameObject);
+    }
+}
