f030a160b223eac7e65e1015cc567c02af59e17c
Priyank <ppraja20@my.centennialcollege.ca>
2020-03-13 13:58:26 -0400

    Added Player Explosion

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -5,9 +5,15 @@ using UnityEngine;
 public class DestroyByContact : MonoBehaviour
 {
     public GameObject Explosion;
+    public GameObject PlayerExplosion;
     void OnCollisionEnter2D(Collision2D other)
     {
         Instantiate(Explosion, transform.position, transform.rotation);
+
+        if(other.gameObject.CompareTag("Player"))
+        {
+            Instantiate(PlayerExplosion, other.transform.position, other.transform.rotation);
+        }
 
         Destroy(other.gameObject);
         Destroy(this.gameObject);
